a04965c0789d8d93b0066180da0b33e679ceeb41
joybiswasmritunjoy <contact.joybiswas@gmail.com>
2021-10-20 06:13:01 -0700

    Done

--- src/Components/Pages/404/NotFound404.js
+++ src/Components/Pages/404/NotFound404.js
@@ -3,7 +3,8 @@ import React from 'react';
 const NotFound404 = () => {
     return (
         <div>
-            Page Not Found
+            <img src="https://previews.123rf.com/images/amisb/amisb1702/amisb170200205/73266369-page-not-found-error-404-.jpg" alt=""/>
+            <button><a href="https://hospital-joy.web.app/">Go Back</a></button>
         </div>
     );
 };
